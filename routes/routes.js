fac47c66e5b18e6b19c2acfb976e23e21e8b9cc3
Pavel Maslov <pavel.masslove@gmail.com>
2020-06-27 13:26:17 +0300

    Добавлен главный файл роутов

---
+++ routes/routes.js
@@ -0,0 +1,14 @@
+// routes/routes.js
+
+const router = require('express').Router();
+const usersRouter = require('./user');
+const articlesRouter = require('./articles');
+const NotFoundError = require('../errors/not-found-err');
+
+router.use('/users', usersRouter);
+router.use('/articles', articlesRouter);
+router.all('*', () => {
+  throw new NotFoundError('Такой ресурс не доступен');
+});
+
+module.exports = router;
